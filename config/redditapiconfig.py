3d5f39784e5d90066f1d614a9aba7ed3a9697030
Emad Bin Abid <emad.bin.abid@gmail.com>
2020-06-27 00:28:50 +0500

    Added configurations for Reddit API.

---
+++ config/redditapiconfig.py
@@ -0,0 +1,10 @@
+# Reddit API configuration
+
+def get_redditapi_config():
+    return {
+        'CLIENT_ID': '2gj8w8W6ooHmmw',
+        'CLIENT_SECRET': '63jGYd3HTlIQcYEBLKRItSQYSfQ',
+        'USERNAME': 'newzology',
+        'PASSWORD': '',
+        'USER_AGENT': 'newzologyv1'
+    }
