3655295e2501c2f99fbb4ec33cdd62647bc98f4b
Emad Bin Abid <emad.bin.abid@gmail.com>
2020-06-27 20:09:02 +0500

    Converted Logger module into singleton module.

--- nstorelogger/logger.py
+++ nstorelogger/logger.py
@@ -2,36 +2,34 @@ import logging
 
 from config.appconfig import get_log_filename
 
-class Logger:
-    def __init__(self):
-        self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
-        self.fileHandler = logging.FileHandler(get_log_filename())
-        self.fileHandler.setFormatter(formatter)
-
-    def debug(self, message):
-        self.fileHandler.setLevel(logging.DEBUG)
-        self.logger.addHandler(self.fileHandler)
-        self.logger.debug(message)
-
-    def error(self, message):
-        self.fileHandler.setLevel(logging.ERROR)
-        self.logger.addHandler(self.fileHandler)
-        self.logger.error(message)
-
-
-class OnlyOne(object):
-    class __OnlyOne:
+class Logger(object):
+    class __Logger:
         def __init__(self):
             self.val = None
+            self.logger = logging.getLogger(__name__)
+            self.logger.setLevel(logging.DEBUG)
+            formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
+            self.fileHandler = logging.FileHandler(get_log_filename())
+            self.fileHandler.setFormatter(formatter)
+
+        def debug(self, message):
+            self.fileHandler.setLevel(logging.DEBUG)
+            self.logger.addHandler(self.fileHandler)
+            self.logger.debug(message)
+
+        def error(self, message):
+            self.fileHandler.setLevel(logging.ERROR)
+            self.logger.addHandler(self.fileHandler)
+            self.logger.error(message)
+        
         def __str__(self):
             return self.val
+            
     instance = None
     def __new__(cls): # __new__ always a classmethod
-        if not OnlyOne.instance:
-            OnlyOne.instance = OnlyOne.__OnlyOne()
-        return OnlyOne.instance
+        if not Logger.instance:
+            Logger.instance = Logger.__Logger()
+        return Logger.instance
     def __getattr__(self, name):
         return getattr(self.instance, name)
     def __setattr__(self, name):
