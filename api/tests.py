5b6f9251add5ee8e1b7422f8a864189359cba380
Emad Bin Abid <emad.bin.abid@gmail.com>
2020-06-28 19:36:27 +0500

    Added unit test logging.

--- api/tests.py
+++ api/tests.py
@@ -4,9 +4,14 @@ from .models import News, Keywords, NewsHistory, Sources
 from config.appconfig import get_api_token
 from rest_framework.test import APIClient
 
-class TestUniversal(unittest.TestCase):
+from nstorelogger.logger import Logger
+log = Logger()
+
+class TestApiView(unittest.TestCase):
     def test_get_news_list(self):
         # Test with auth
+        log.debug('Running test on GET /news with auth')
+
         client = APIClient()
         client.credentials(HTTP_AUTHORIZATION=get_api_token())
         response = client.get('/news')
@@ -15,16 +20,21 @@ class TestUniversal(unittest.TestCase):
         apiResponse = apiResponse.data
         for item in apiResponse:
             self.assertEqual(list(item.keys()), ['headline', 'link', 'source_id_id'])
+        log.debug('Test ended')
 
         # Test without auth
+        log.debug('Running test on GET /news without auth')
         client = APIClient()
         response = client.get('/news')
         request = response.wsgi_request
         apiResponse = news_list(request)
         self.assertEqual(apiResponse.status_code, 401)
+        log.debug('Test ended')
 
     def test_get_sources_list(self):
         # Test with auth
+        log.debug('Running test on GET /sources with auth')
+        
         client = APIClient()
         client.credentials(HTTP_AUTHORIZATION=get_api_token())
         response = client.get('/sources')
@@ -33,18 +43,23 @@ class TestUniversal(unittest.TestCase):
         apiResponse = apiResponse.data
         for item in apiResponse:
             self.assertEqual(list(item.keys()), ['id', 'source'])
+        log.debug('Test ended')
 
         # Test without auth
+        log.debug('Running test on GET /sources without auth')
         client = APIClient()
         response = client.get('/sources')
         request = response.wsgi_request
         apiResponse = sources_list(request)
         self.assertEqual(apiResponse.status_code, 401)
+        log.debug('Test ended')
     
     def test_get_keyword_id(self):
+        log.debug('Running test for checking valid keyword id')
         keywordsList = Keywords.objects.filter(keyword='general').values('id')
         keywordIdList = list()
         for keyword in keywordsList:
             keywordIdList.append(keyword['id'])
         
         self.assertIn(getKeywordId('general'), keywordIdList)
+        log.debug('Test ended')
--- external/test_apinews.py
+++ external/test_apinews.py
@@ -1,8 +1,13 @@
 import unittest
 from . import apinews
 
+from nstorelogger.logger import Logger
+log = Logger()
+
 class TestNewsApi(unittest.TestCase):
     def test_get_news(self):
+        log.debug('Running test on NewsAPI get news call')
         listFromApiResponse = apinews.NewsApi('general').getNews()
         for item in listFromApiResponse:
             self.assertEqual(list(item.keys()), ['headline', 'link', 'source'])
+        log.debug('Test ended')
