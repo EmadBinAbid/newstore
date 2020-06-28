2b907ff5ca76b27803fafc721e2ae6fca553cf2c
Emad Bin Abid <emad.bin.abid@gmail.com>
2020-06-28 18:31:48 +0500

    Added filename for test logs.

--- config/appconfig.py
+++ config/appconfig.py
@@ -10,6 +10,9 @@ def get_data_expiry_timedelta() -> dict:
 def get_log_filename() -> str:
     return 'logs/app.log'
 
+def get_testlog_filename() -> str:
+    return 'logs/test.log'
+
 def is_news_history_table_active() -> bool:
     return True
 
